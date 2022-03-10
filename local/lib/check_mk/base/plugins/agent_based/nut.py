9dbca6c6019056f9bd63f9fb94d3d8cb5989d753
Daniel Martin Gonzalez <danimart1991@outlook.com>
2022-03-10 16:17:27 +0100

    Add Variables naming and grouping

--- local/lib/check_mk/base/plugins/agent_based/nut.py
+++ local/lib/check_mk/base/plugins/agent_based/nut.py
@@ -11,14 +11,54 @@ def check_nut(item, section):
     section = clean_section(section)
     for ups_name, variable, value in section:
         if title_generator(ups_name, variable) == item:
-            yield Result(state=State.OK, summary=value)
-            return
+            if variable == "Load":
+                yield from check_load(value)
+                return
+            if variable == "Output Voltage":
+                yield from check_output_voltage(value)
+                return
+            else:
+                yield Result(state=State.OK, summary=value)
+                return
+
+
+def check_load(value):
+    value = int(value)
+    value_warn = 50
+    value_crit = 70
+    yield Metric("load", value, levels=(value_warn, value_crit))
+
+    state = State.OK
+    if value >= value_crit:
+        state = State.CRIT
+    elif value >= value_warn:
+        state = State.WARN
+    yield Result(state=state, summary=f"Load on UPS: {render.percent(value)}")
+
+
+def check_output_voltage(value):
+    value = float(value)
+    value_warn = 245
+    value_crit = 250
+    yield Metric("output_voltage", value, levels=(value_warn, value_crit))
+
+    state = State.OK
+    if value >= value_crit:
+        state = State.CRIT
+    elif value >= value_warn:
+        state = State.WARN
+    yield Result(state=state, summary=f"Output Voltage: {value} V")
 
 
 def clean_section(section):
     items = []
     for line in section:
-        items.append([line[0], line[1][:-1], ' '.join(line[2:])])
+        variable = line[1][:-1]
+        if (variable == "ups.load"):
+            variable = "Load"
+        elif (variable == "output.voltage"):
+            variable = "Output Voltage"
+        items.append([line[0], variable, ' '.join(line[2:])])
     return items
 
 
